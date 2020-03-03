be3fdd1308d24adbaf7f92a2ccec5047b0791a6b
Garrett James <garrettjames55@gmail.com>
2020-03-03 10:44:28 -0500

    finished app functionality

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+$(() => {
+  BOOKMARKS.bindEventListeners();
+  API.getAllBookmarks()
+    .then(allBookmarks => {
+      allBookmarks.forEach(bookmark => STORE.add(bookmark));
+      BOOKMARKS.render();
+    })
+    .catch(error => console.error(`${error.code} ${error.message}`));
+});
